7d06db558f14d429c74df70feeac28c2b2919e83
Tosho Todorovski <tose_todorovski@yahoo.com>
2022-06-12 15:30:06 +0200

    Disable and comment out UseSpa method to fix build error

--- Cleverbit.RegionsWithApi.WebApi/Cleverbit.RegionsWithApi.WebApi/Configuration/InstallerExtensions.cs
+++ Cleverbit.RegionsWithApi.WebApi/Cleverbit.RegionsWithApi.WebApi/Configuration/InstallerExtensions.cs
@@ -62,13 +62,13 @@ namespace Cleverbit.RegionsWithApi.WebApi.Configuration
 
             application.UseErrorHandlingFilter();
 
-            application.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+            //application.UseSpa(spa =>
+            //{
+            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+            //    // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
-            });
+            //    spa.Options.SourcePath = "ClientApp";
+            //});
         }
     }
 }
